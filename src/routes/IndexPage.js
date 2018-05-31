03fb7ce4aec0e744de364d8b1cfe5266a4dfa705
Chen Jian <lsdlab@icloud.com>
2018-05-31 10:35:38 +0800

    update

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { connect } from 'dva';
-import styles from './IndexPage.css';
 
 function IndexPage() {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
+    <div>
+      <nav className="navbar navbar-light">
+        <div className="container">
+          <a className="navbar-brand" href="/">conduit</a>
+          <ul className="nav navbar-nav pull-xs-right">
+            <li className="nav-item">
+              <a className="nav-link active" href="">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                <i className="ion-compose"></i>&nbsp;New Post
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                <i className="ion-gear-a"></i>&nbsp;Settings
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">Sign up</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <footer>
+        <div className="container">
+          <a href="/" className="logo-font">conduit</a>
+          <span className="attribution">
+            An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+          </span>
+        </div>
+      </footer>
     </div>
   );
 }
